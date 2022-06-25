1b1183a50fe498b2d00b204eef61fb642be53fd0
Paweł Wegner <pawel.wegner95@gmail.com>
2022-06-25 15:28:14 +0200

    coro-cloudstorage: updated.

--- src/coro/cloudstorage/fuse/filesystem_context.cc
+++ src/coro/cloudstorage/fuse/filesystem_context.cc
@@ -8,12 +8,7 @@ namespace coro::cloudstorage::fuse {
 
 namespace {
 
-using ::coro::cloudstorage::util::AbstractCloudFactory;
-using ::coro::cloudstorage::util::AuthTokenManager;
-using ::coro::cloudstorage::util::CreateAbstractCloudProvider;
 using ::coro::cloudstorage::util::GetConfigFilePath;
-using ::coro::cloudstorage::util::MergedCloudProvider;
-using ::coro::cloudstorage::util::SettingsManager;
 
 struct ForwardToMergedCloudProvider {
   void OnCreate(util::CloudProviderAccount* account) {
@@ -28,33 +23,16 @@ struct ForwardToMergedCloudProvider {
   util::MergedCloudProvider* provider;
 };
 
-auto GetSettingsManager(AbstractCloudFactory* factory,
-                        std::optional<std::string> config_path) {
-  std::string path = [&] {
-    if (config_path) {
-      return std::move(*config_path);
-    } else {
-      return GetConfigFilePath();
-    }
-  }();
-  return SettingsManager(AuthTokenManager{factory, path}, path);
-}
-
 }  // namespace
 
 FileSystemContext::FileSystemContext(const coro::util::EventLoop* event_loop,
                                      Config config)
-    : context_(event_loop),
+    : context_(event_loop,
+               std::move(config).config_path.value_or(GetConfigFilePath())),
       provider_(CreateAbstractCloudProviderImpl(&merged_provider_)),
-      timing_out_provider_(context_.event_loop(), config.timeout_ms,
-                           &provider_),
+      timing_out_provider_(event_loop, config.timeout_ms, &provider_),
       fs_(&timing_out_provider_, context_.thread_pool(), config.fs_config),
-      http_server_(context_.event_loop(),
-                   GetSettingsManager(context_.factory(), config.config_path)
-                       .GetHttpServerConfig(),
-                   context_.factory(), context_.thumbnail_generator(),
-                   ForwardToMergedCloudProvider{&merged_provider_},
-                   GetSettingsManager(context_.factory(), config.config_path)) {
-}
+      http_server_(context_.CreateHttpServer(
+          ForwardToMergedCloudProvider{&merged_provider_})) {}
 
 }  // namespace coro::cloudstorage::fuse
